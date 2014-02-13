ef67b144e3255afdb089fcb3faf3d94233039517
Daniel Alamo <daniel.alamo@gmail.com>
2014-02-13 15:32:46 -0500

    Clean Up

--- Pielog/src/view/SnippetJPanel.java
+++ Pielog/src/view/SnippetJPanel.java
@@ -1,32 +1,23 @@
 package view;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.SystemColor;
-import javax.swing.border.LineBorder;
-import java.awt.Component;
-import java.awt.Color;
-import java.awt.event.FocusListener;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
+import javax.swing.JPanel;
 import javax.swing.OverlayLayout;
 import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
 
 public class SnippetJPanel extends JPanel {
 
 	private static final long serialVersionUID = 2369647289651271374L;
-	private MyJSplitPane myJSplitPane;
-	private String title;
-	private String body;
 	private JPanel overlay;
 	
 	public SnippetJPanel(final String title, final String body, String time, final MyJSplitPane myJSplitPane){
-		this.myJSplitPane = myJSplitPane;
-		this.title = title;
-		this.body = body;
 		setLayout(new OverlayLayout(this)); 
 		setMaximumSize(new Dimension(3000, 80));
 		setMinimumSize(new Dimension(0, 0));
@@ -39,15 +30,6 @@ public class SnippetJPanel extends JPanel {
 		overlay = new JPanel();
 		overlay.addMouseListener(new MouseAdapter(){
 			@Override
-			public void mouseEntered(MouseEvent e) {
-				overlay.setBackground(new Color(255,255,255,60));
-				
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {
-				overlay.setBackground(new Color(255,255,255,0));
-			}
-			@Override
 			public void mousePressed(MouseEvent e) { 
 				myJSplitPane.mainText.setText(body);
 				if (!title.isEmpty()) {
@@ -55,45 +37,15 @@ public class SnippetJPanel extends JPanel {
 					myJSplitPane.txtArticleTitle.setText(title);
 				}
 			}
-
 		});
-		
-		overlay.setOpaque(true);
+		overlay.setOpaque(false);
 		overlay.setBackground(SystemColor.inactiveCaption);
 		add(overlay);  
 		
 		JPanel snippetBuild = new SnippetBuildJPanel(title, body, time);
-		
-		/////////
-		
-		
-		
-		/////////
 		add(snippetBuild);
 		
 	}
-
-//	
-//	@Override
-//	public void mousePressed(MouseEvent e) { 
-//		this.myJSplitPane.mainText.setText(this.body);
-//		if (!this.title.isEmpty()) {
-//			this.myJSplitPane.txtArticleTitle.setForeground(Color.BLACK);
-//			this.myJSplitPane.txtArticleTitle.setText(this.title);
-//		}
-//	}
-//	
-//	@Override
-//	public void mouseClicked(MouseEvent e) { }
-//
-//	@Override
-//	public void mouseEntered(MouseEvent e) { }
-//
-//	@Override
-//	public void mouseExited(MouseEvent e) { }
-//	
-//	@Override
-//	public void mouseReleased(MouseEvent e) { }
 
 }
 
